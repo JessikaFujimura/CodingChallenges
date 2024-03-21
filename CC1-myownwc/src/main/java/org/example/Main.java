8d721ced4021d4c3a084ada50d529e8b671c5c60
jessika <jessikafujimura@gmail.com>
2024-03-20 23:02:54 -0300

    feat: step three count number of words

--- CC1-myownwc/src/main/java/org/example/Main.java
+++ CC1-myownwc/src/main/java/org/example/Main.java
@@ -1,11 +1,15 @@
 package org.example;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,6 +29,33 @@ public class Main {
                 } catch (IOException ex){
                     ex.printStackTrace();
                 }
+                break;
+            case "-w" :
+                long words = 0;
+                try {
+                    Stream<String> fileLines = Files.lines(Paths.get(nameFile), Charset.defaultCharset());
+                    words= fileLines.flatMap(l -> Arrays.stream(l.split("\\s+"))
+                            .filter(i -> !i.isBlank() || !i.isEmpty()))
+                            .count();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.printf("%d %s\n", words, nameFile);
+                // Primeira Resolução
+//                try(LineNumberReader numberReader = new LineNumberReader(new FileReader(file))){
+//                    words = 0;
+//                    String currentLine = numberReader.readLine();
+//                    while ( currentLine != null ){
+//                        if(!currentLine.isEmpty() && !currentLine.isBlank()){
+//                            String[] lineWords = currentLine.strip().split("\\s+");
+//                            words += lineWords.length;
+//                        }
+//                        currentLine = numberReader.readLine();
+//                    }
+//                    System.out.printf("%d %s\n", words,nameFile );
+//                } catch (IOException ex){
+//                    ex.printStackTrace();
+//                }
                 break;
         }
     }
